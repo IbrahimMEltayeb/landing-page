8602c7265d855d6f37171e64a32bca7e978b9c80
IbrahimMEltayeb <ibrahimm.eltayebie@gmail.com>
2024-05-17 16:44:27 +0300

    update the responsive of nav-bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import "./Navbar.css";
+import pic from "./NewTux.png"
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
       <div className="topbarContainer">
+
         <div className="topbarLeft">
-          <img src="./NewTux.png" alt="profile pic" className="topbarImg" />
-          <div className="topbarIcons">
-            <div className="topbarIconItem">
-              <FavoriteIcon />
-              <span className="topbarIconBadge">المفضلة</span>
+          <img src={pic} title="profile pic" alt="profile pic" className="topbarImg" />
+            <div className={`topbarLinks ${menuOpen ? "showMenu" : ""}`}>
+              <div className="topbarIcons">
+                <div className="topbarIconItem">
+                  <FavoriteIcon />
+                  <span className="topbarIconBadge">المفضلة</span>
+                </div>
+                <div className="topbarIconItem">
+                  <ShoppingCartIcon />
+                  <span className="topbarIconBadge">سلة التسوق</span>
+                </div>
+              </div>
+              <span className="topbarLink">الرئيسية</span>
+              <span className="topbarLink">طلبياتى</span>
             </div>
-            <div className="topbarIconItem">
-              <ShoppingCartIcon />
-              <span className="topbarIconBadge">سلة التسوق</span>
-            </div>
-          </div>
-          <div className="topbarLinks">
-            <span className="topbarLink">الرئيسية</span>
-            <span className="topbarLink">طلبياتى</span>
-          </div>
         </div>
         <div className="topbarCenter">
           <div className="searchbar">
@@ -34,10 +44,16 @@ function Navbar() {
             />
           </div>
         </div>
+        
         <div className="topbarRight">
           <span className="logo">اللوجو</span>
+          <button className="menuButton" onClick={toggleMenu}>
+            {menuOpen ? <CloseIcon /> : <MenuIcon />}
+          </button>
         </div>
+
       </div>
+
     </div>
   );
 }
